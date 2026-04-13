2ed79a65edd91d070a22bfba7406788d093d56c4
CarlosRuz <carlosruz@upmy.co>
2026-04-13 15:15:12 -0600

    /fix mejorar el estilo de los mensajes de toast en GlobalToast

--- src/components/GlobalToast.tsx
+++ src/components/GlobalToast.tsx
@@ -1,29 +1,44 @@
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 
 export class GlobalToast {
-  constructor(readonly message: string, readonly type: "success" | "error" | "info") {}
+  constructor(
+    readonly message: string,
+    readonly type: "success" | "error" | "info",
+  ) {}
 
   static success(message: string) {
     return toast.success(message, {
       style: {
         background: "#2C3E3A",
         width: "fit-content",
-        padding: "8px 16px",
+        maxWidth: "90vw",
+        minWidth: "60vw",
+        margin: "8px",
+        wordBreak: "break-word",
       },
     });
   }
+
   static error(message: string) {
-    return toast.error(message , {style : {
-      width: "fit-content",
-        padding: "8px 16px",
-    }});
+    return toast.error(message, {
+      style: {
+        width: "fit-content",
+        maxWidth: "90vw",
+        minWidth: "60vw",
+        margin: "8px",
+        wordBreak: "break-word",
+      },
+    });
   }
 
   static info(message: string) {
     return toast.info(message, {
       style: {
         width: "fit-content",
-        margin: "8px 16px",
+        maxWidth: "90vw",
+        minWidth: "60vw",
+        margin: "8px",
+        wordBreak: "break-word",
       },
     });
   }
